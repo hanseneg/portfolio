13a82be41606e66d9a97f5e159551e8ad8c25532
Ethan Hansen <hanseneg@hotmail.com>
2021-03-16 12:37:01 -0600

    commit6

--- src/Site.js
+++ src/Site.js
@@ -5,22 +5,36 @@ export default function Site() {
         <div>
             <h1>Projects</h1>
             <div>
-                <a href='https://movie-posters.netlify.app/' target="_blank">Movie Poster App</a>
-            </div>
-            <div>
-                <a href='https://meme-generator-fun.netlify.app/' target="_blank">Meme Generator App</a>
-            </div>
-            <div>
-                <a href='https://css-zen-garden-copy.netlify.app/' target="_blank">CSS Zen Garden</a>
-            </div>
-            <div>
-                <a href='https://mario-pest-control.netlify.app/' target="_blank">Mario Pest Control</a>
-            </div>
-            <div>
-                <a target="_blank">Bounty Hunter</a>
-            </div>
-            <div>
-                <a target="_blank">JavaScript Quiz</a>
+                <div>
+                    <a href='https://movie-posters.netlify.app/' target="_blank">Movie Poster App</a>
+                </div>
+                <div>
+                    <a href="https://lh3.googleusercontent.com/uJijmzHTakSFxG0X9Fw6eIgJmXFDmuhip21TjjhrPzIYqCmO9fn9Yag52vrcj5tKIS4WTKQn_wWYb1yncKgHHvkHCoDyvZC3gSCEBbeMwy2X2l0MxHhS7g3_VU92wJjKROfF2zYq=w2400?source=screenshot.guru"> <img src="https://lh3.googleusercontent.com/uJijmzHTakSFxG0X9Fw6eIgJmXFDmuhip21TjjhrPzIYqCmO9fn9Yag52vrcj5tKIS4WTKQn_wWYb1yncKgHHvkHCoDyvZC3gSCEBbeMwy2X2l0MxHhS7g3_VU92wJjKROfF2zYq=w600-h315-p-k" /> </a>
+                </div>
+                <div>
+                    <a href='https://meme-generator-fun.netlify.app/' target="_blank">Meme Generator App</a>
+                </div>
+                <div>
+                    <a href="https://lh3.googleusercontent.com/bOPInH-XOBYCw1pS4UObwPWjEA6rVyIWch204uijGd1cNmGMHewAFNsytWczGHlnu25N-RbaVzNeiM1LWhsYBPtKisdkcbN6q_DyLf8Mmr8p6wbSXBrGMxoqGA9E2VGnEuRvZ9yU=w2400?source=screenshot.guru"> <img src="https://lh3.googleusercontent.com/bOPInH-XOBYCw1pS4UObwPWjEA6rVyIWch204uijGd1cNmGMHewAFNsytWczGHlnu25N-RbaVzNeiM1LWhsYBPtKisdkcbN6q_DyLf8Mmr8p6wbSXBrGMxoqGA9E2VGnEuRvZ9yU=w600-h315-p-k" /> </a>
+                </div>
+                <div>
+                    <a href='https://css-zen-garden-copy.netlify.app/' target="_blank">CSS Zen Garden</a>
+                </div>
+                <div>
+                    <a href="https://lh3.googleusercontent.com/_FsIAoleAAmdiV68nh78zSxH_oBg40IDAkoDLh8-PkFO6sbq2pDF0b_8RkMQ-6LNpbsANlbSov05rN4vghrb2W7nx8Pb0eAgIR8yAaMlCiqBkamAIPjchqsBNPGkmTyUs0VL2Ldb=w2400?source=screenshot.guru"> <img src="https://lh3.googleusercontent.com/_FsIAoleAAmdiV68nh78zSxH_oBg40IDAkoDLh8-PkFO6sbq2pDF0b_8RkMQ-6LNpbsANlbSov05rN4vghrb2W7nx8Pb0eAgIR8yAaMlCiqBkamAIPjchqsBNPGkmTyUs0VL2Ldb=w600-h315-p-k" /> </a>
+                </div>
+                <div>
+                    <a href='https://mario-pest-control.netlify.app/' target="_blank">Mario Pest Control</a>
+                </div>
+                <div>
+                    <a href="https://lh3.googleusercontent.com/qVVEwVgiELRcwTQnW0hJjEAJ79JXHYisV48hBn--tNX4WzKk9vJ5pKWJfT-FHok9RMvO-hCm5wP1GZAAl0bY6EqnbF1n_ECDMQZYkrxBeHrPHVfx-hJFTrI4oO6Q7JKKUpBnFbR7=w2400?source=screenshot.guru"> <img src="https://lh3.googleusercontent.com/qVVEwVgiELRcwTQnW0hJjEAJ79JXHYisV48hBn--tNX4WzKk9vJ5pKWJfT-FHok9RMvO-hCm5wP1GZAAl0bY6EqnbF1n_ECDMQZYkrxBeHrPHVfx-hJFTrI4oO6Q7JKKUpBnFbR7=w600-h315-p-k" /> </a>
+                </div>
+                {/* <div>
+                    <a target="_blank">Bounty Hunter</a>
+                </div>
+                <div>
+                    <a target="_blank">JavaScript Quiz</a>
+                </div> */}
             </div>
         </div>
     )
