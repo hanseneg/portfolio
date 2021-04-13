85e6c36b8efdf7d608cfa0ba4878894ee2cf5b33
Ethan Hansen <hanseneg@hotmail.com>
2021-04-13 09:37:03 -0600

    commit12

--- src/Contact.js
+++ src/Contact.js
@@ -3,16 +3,16 @@ import React from 'react'
 export default function Contact() {
     return (
         <div className='work-icons'>
-            <i style={{color: "#0072b1"}} className='icons' class="fab fa-linkedin fa-4x"></i>
+            <i /* style={{color: "#0072b1"}} */ className='icons' class="fab fa-linkedin fa-4x"></i>
             <a className='icons' href='https://www.linkedin.com/in/ethan-hansen-869332181/'>LinkedIn</a>
-            <i style={{color: "211F1F"}} className='icons' class="fab fa-github fa-4x"></i>
+            <i /* style={{color: "211F1F"}} */ className='icons' class="fab fa-github fa-4x"></i>
             <a className='icons' href='https://github.com/hanseneg'>Github</a>
-            <i style={{color: "black"}} className='icons' class="far fa-file fa-4x"></i>
+            <i /* style={{color: "black"}} */ className='icons' class="far fa-file-alt fa-4x"></i>
+            {/* <span className="material-icons">article</span> */}
             <a className='icons' href='https://docs.google.com/document/d/1GT4J6YbtbXTvmqeKYb9eubVyZ5KF4Ft4U4_AMoYHAxc/edit?usp=sharing'>Resume</a>
-            
-            <i style={{color: "rgb(52, 199, 89)"}} className='icons'  class="fas fa-phone-square fa-4x"></i>
+            <i /* style={{color: "rgb(52, 199, 89)"}} */ className='icons'  class="fas fa-phone-square fa-4x"></i>
             <p className='icons'>208-650-2331</p>
-            <i style={{color: "#0072B5"}} className='icons'  class="fas fa-envelope-square fa-4x"></i>
+            <i /* style={{color: "#0072B5"}} */ className='icons'  class="fas fa-envelope-square fa-4x"></i>
             <p className='icons'>hanseneg@hotmail.com</p>
         </div>
     )
