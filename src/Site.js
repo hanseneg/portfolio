60fa762d5484a92e6b57b26d71db6ccdb36aa304
Ethan Hansen <hanseneg@hotmail.com>
2021-04-13 16:04:30 -0600

    commit14

--- src/Site.js
+++ src/Site.js
@@ -9,28 +9,28 @@ export default function Site() {
                     <a href='https://movie-posters.netlify.app/' target="_blank">Movie Poster App</a>
                     <p>App that allows you to search for your favorite movie posters using an API. Built using React.</p>
                 </div>
-                <div div className='two'>
+                <div className='two'>
                     <a href="https://lh3.googleusercontent.com/uJijmzHTakSFxG0X9Fw6eIgJmXFDmuhip21TjjhrPzIYqCmO9fn9Yag52vrcj5tKIS4WTKQn_wWYb1yncKgHHvkHCoDyvZC3gSCEBbeMwy2X2l0MxHhS7g3_VU92wJjKROfF2zYq=w2400?source=screenshot.guru"> <img src="https://lh3.googleusercontent.com/uJijmzHTakSFxG0X9Fw6eIgJmXFDmuhip21TjjhrPzIYqCmO9fn9Yag52vrcj5tKIS4WTKQn_wWYb1yncKgHHvkHCoDyvZC3gSCEBbeMwy2X2l0MxHhS7g3_VU92wJjKROfF2zYq=w600-h315-p-k" /> </a>
                 </div>
-                <div div className='three'>
+                <div className='three'>
                     <a href='https://meme-generator-fun.netlify.app/' target="_blank">Meme Generator App</a>
                     <p>An app that randomly selects meme images from an API and then allows you to place wording on them and save them to create your own memes. Built with a team using React.</p>
                 </div>
-                <div div className='four'>
+                <div className='four'>
                     <a href="https://lh3.googleusercontent.com/bOPInH-XOBYCw1pS4UObwPWjEA6rVyIWch204uijGd1cNmGMHewAFNsytWczGHlnu25N-RbaVzNeiM1LWhsYBPtKisdkcbN6q_DyLf8Mmr8p6wbSXBrGMxoqGA9E2VGnEuRvZ9yU=w2400?source=screenshot.guru"> <img src="https://lh3.googleusercontent.com/bOPInH-XOBYCw1pS4UObwPWjEA6rVyIWch204uijGd1cNmGMHewAFNsytWczGHlnu25N-RbaVzNeiM1LWhsYBPtKisdkcbN6q_DyLf8Mmr8p6wbSXBrGMxoqGA9E2VGnEuRvZ9yU=w600-h315-p-k" /> </a>
                 </div>
-                <div div className='five'>
+                <div className='five'>
                     <a href='https://css-zen-garden-copy.netlify.app/' target="_blank">CSS Zen Garden</a>
                     <p>Copy of a site built using only CSS and HTML that shows what is possible with CSS. It was originally created in 2003 by Dave Shea.</p>
                 </div>
-                <div div className='six'>
+                <div className='six'>
                     <a href="https://lh3.googleusercontent.com/_FsIAoleAAmdiV68nh78zSxH_oBg40IDAkoDLh8-PkFO6sbq2pDF0b_8RkMQ-6LNpbsANlbSov05rN4vghrb2W7nx8Pb0eAgIR8yAaMlCiqBkamAIPjchqsBNPGkmTyUs0VL2Ldb=w2400?source=screenshot.guru"> <img src="https://lh3.googleusercontent.com/_FsIAoleAAmdiV68nh78zSxH_oBg40IDAkoDLh8-PkFO6sbq2pDF0b_8RkMQ-6LNpbsANlbSov05rN4vghrb2W7nx8Pb0eAgIR8yAaMlCiqBkamAIPjchqsBNPGkmTyUs0VL2Ldb=w600-h315-p-k" /> </a>
                 </div>
-                <div div className='seven'>
+                <div className='seven'>
                     <a href='https://mario-pest-control.netlify.app/' target="_blank">Mario Pest Control</a>
                     <p>A simple app that calculates how much you will be paid by Princess Peach for catching the baddies in the Mario universe. Built using Javascript.</p>
                 </div>
-                <div div className='eight'>
+                <div className='eight'>
                     <a href="https://lh3.googleusercontent.com/qVVEwVgiELRcwTQnW0hJjEAJ79JXHYisV48hBn--tNX4WzKk9vJ5pKWJfT-FHok9RMvO-hCm5wP1GZAAl0bY6EqnbF1n_ECDMQZYkrxBeHrPHVfx-hJFTrI4oO6Q7JKKUpBnFbR7=w2400?source=screenshot.guru"> <img src="https://lh3.googleusercontent.com/qVVEwVgiELRcwTQnW0hJjEAJ79JXHYisV48hBn--tNX4WzKk9vJ5pKWJfT-FHok9RMvO-hCm5wP1GZAAl0bY6EqnbF1n_ECDMQZYkrxBeHrPHVfx-hJFTrI4oO6Q7JKKUpBnFbR7=w600-h315-p-k" /> </a>
                 </div>
                 <div className='nine'>
