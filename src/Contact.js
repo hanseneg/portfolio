28d80aca0d25ea8c8dbe25ae8110e205d594674b
Ethan Hansen <hanseneg@hotmail.com>
2021-07-12 13:23:22 -0600

    commit30

--- src/Contact.js
+++ src/Contact.js
@@ -11,8 +11,8 @@ export default function Contact() {
                 <a target="_blank" href='https://github.com/hanseneg'><i className='icons' target="_blank" class="fab fa-github fa-4x"></i></a>
                 <a className='icons' target="_blank" href='https://github.com/hanseneg'>Github</a>
             
-                <a target="_blank" href='https://docs.google.com/document/d/115EVziko5RCjsEuTFM9EDfT2x6bwnGX4TprM-OrwsUs/edit#'><i className='icons' target="_blank" class="far fa-file-alt fa-4x"></i></a>
-                <a className='icons' target="_blank" href='https://docs.google.com/document/d/115EVziko5RCjsEuTFM9EDfT2x6bwnGX4TprM-OrwsUs/edit#'>Resume</a>
+                <a target="_blank" href='https://drive.google.com/file/d/1uF2O0faXkbyuav75Kx1dlZus5VAa9zGI/view?usp=sharing'><i className='icons' target="_blank" class="far fa-file-alt fa-4x"></i></a>
+                <a className='icons' target="_blank" href='https://drive.google.com/file/d/1uF2O0faXkbyuav75Kx1dlZus5VAa9zGI/view?usp=sharing'  /* google doc link 'https://docs.google.com/document/d/115EVziko5RCjsEuTFM9EDfT2x6bwnGX4TprM-OrwsUs/edit#' */>Resume</a>
             
                 <i className='icons'  class="fas fa-phone-square fa-4x"></i>
                 <p className='icons'>208-650-2331</p>
