dadee5d0934e3f4b6de7d64f4a2e4c632887515d
Ethan Hansen <hanseneg@hotmail.com>
2021-08-24 14:54:25 -0600

    commit31

--- src/Contact.js
+++ src/Contact.js
@@ -14,11 +14,11 @@ export default function Contact() {
                 <a target="_blank" href='https://drive.google.com/file/d/1uF2O0faXkbyuav75Kx1dlZus5VAa9zGI/view?usp=sharing'><i className='icons' target="_blank" class="far fa-file-alt fa-4x"></i></a>
                 <a className='icons' target="_blank" href='https://drive.google.com/file/d/1uF2O0faXkbyuav75Kx1dlZus5VAa9zGI/view?usp=sharing'  /* google doc link 'https://docs.google.com/document/d/115EVziko5RCjsEuTFM9EDfT2x6bwnGX4TprM-OrwsUs/edit#' */>Resume</a>
             
-                <i className='icons'  class="fas fa-phone-square fa-4x"></i>
-                <p className='icons'>208-650-2331</p>
+                <a href="tel:2086502331"> <i className='icons'  class="fas fa-phone-square fa-4x"></i> </a>
+                <p className='icons'><a href="tel:2086502331">2086502331</a></p>
             
-                <i className='icons'  class="fas fa-envelope-square fa-4x"></i>
-                <p className='icons'>hanseneg@hotmail.com</p>
+                <a href="mailto:hanseneg@hotmail.com.com"><i className='icons'  class="fas fa-envelope-square fa-4x"></i></a>
+                <p className='icons'><a href="mailto:hanseneg@hotmail.com.com">hanseneg@hotmail.com</a></p>
             
 
         </div>
